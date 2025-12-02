8b8c0071281a78d25e0e763c99d138f9dcf31dc9
AayushKThakur <connecttoakt@gmail.com>
2025-12-02 17:36:44 +0530

    tailwindcss

--- week-13-tailwind/src/App.jsx
+++ week-13-tailwind/src/App.jsx
@@ -1,12 +1,47 @@
-import { Sidebar } from "./components/sidebar";
+import { useState } from "react";
 import "./App.css";
+import { SidebarToggle } from "./icons/SidebarToggle";
 
 function App() {
+  const [sidebarOpen, setSidebarOpen] = useState(true);
+
   return (
-    <>
-      <Sidebar />
-    </>
+    <div className="flex">
+      <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+      <MainContent sidebarOpen={sidebarOpen} />
+    </div>
   );
 }
+function Sidebar({ sidebarOpen, setSidebarOpen }) {
+  return <div className="fixed top-0 left-0"></div>;
+  if (sidebarOpen) {
+    return (
+      <div className="w-96 h-screen bg-red-100">
+        <div>
+          <div
+            className="cursor-pointer hover:bg=slate-200"
+            onClick={() => {
+              setSidebarOpen(!sidebarOpen);
+            }}
+          >
+            <SidebarToggle />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
+function MainContent() {
+  return (
+    <div className="w-full">
+      <div className="h-72 bg-black"></div>
+      <div className="grid grid-cols-11 gap-8 p-8">
+        <div className="h-96 rounded-2xl bg-red-200 col-span-2 -translate-y-24 shadow-lg"></div>
+        <div className="h-96 rounded-2xl bg-green-200 col-span-6 shadow-lg"></div>
+        <div className="h-96 rounded-2xl bg-yellow-200 col-span-3 shadow-lg"></div>
+      </div>
+    </div>
+  );
+}
 export default App;
